8802dab7e4e688f5fe8501458b8beeacdcf3f586
Erik Björn <erik.g.bjoern@gmail.com>
2020-11-28 21:42:18 +0100

    correct cors settings to work with local client

--- app.js
+++ app.js
@@ -1,53 +1,57 @@
 const bodyParser = require("body-parser");
-const cors = require('cors')
+const cors = require("cors");
 const express = require("express");
 const models = require("./models");
 const passport = require("passport");
 const resolvers = require("./resolvers");
 const typeDefs = require("./typeDefs");
-const session = require('express-session');
+const session = require("express-session");
 const { ApolloServer } = require("apollo-server-express");
-const { GraphQLLocalStrategy, buildContext } = require('graphql-passport')
+const { GraphQLLocalStrategy, buildContext } = require("graphql-passport");
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
+passport.deserializeUser(function (user, done) {
   done(null, user);
 });
 
 passport.use(
   new GraphQLLocalStrategy(async (email, password, done) => {
-    const matchingUser = await models.User.findOne({ where: { email, password } });
-    const error = matchingUser ? null : new Error("incorrect email or password");
+    const matchingUser = await models.User.findOne({
+      where: { email, password },
+    });
+    const error = matchingUser
+      ? null
+      : new Error("incorrect email or password");
     done(error, matchingUser);
   })
 );
 
 const app = express();
+const corsOptions = { origin: "http://localhost:8080", credentials: true }
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({
-  secret: 'reallysecret',
-  resave: false,
-  saveUninitialized: true,
-}));
+app.use(
+  session({
+    secret: "reallysecret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
-app.use(passport.session())
-app.use(cors())
+app.use(passport.session());
+app.use(cors(corsOptions));
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req, res }) => buildContext({ req, res }),
-  cors: {
-    origin: '*',
-  }
-})
+});
 
-server.applyMiddleware({ app });
+server.applyMiddleware({ app, cors: corsOptions });
 
 app.listen(3000, () =>
   console.log("Server listening on localhost:3000/graphql")
