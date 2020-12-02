1dcc08872a9eecc7c079a61b0f1033a7138f98ca
Erik Björn <erik.g.bjoern@gmail.com>
2020-12-02 04:53:57 +0100

    correct database config use_env_variable

--- database/config/config.js
+++ database/config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     "logging": false
   },
   "production": {
-    "url": process.env.DATABASE_URL,
+    "use_env_variable": process.env.DATABASE_URL,
     "dialect": "postgres"
   }
 }
